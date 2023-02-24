a1b8cd54453ed29c4182ccd87942750b1fe9ef17
Amelia <amelia.awuor@gmail.com>
2023-02-24 13:45:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] ;
 
-// Add your functions and code here
+function destructivelyAppendKitten (name){
+
+    kittens.push(name);
+}
+function destructivelyPrependKitten (name){
+    kittens.unshift(name);
+}
+function destructivelyRemoveFirstKitten (name){
+    kittens.shift(name);
+}
+
+function destructivelyRemoveLastKitten (name){
+    kittens.pop(name);
+}
+function appendKitten(name){
+    return kittens.concat(name);
+}
+function prependKitten(name){
+    const newKittens = [name, ...kittens];
+    return newKittens;
+}
+function removeLastKitten(){
+    var caats= kittens.slice(0,kittens.length-1);
+    return caats;
+}
+function removeFirstKitten(){
+    var catts= kittens.slice(1)
+    return catts
+}
